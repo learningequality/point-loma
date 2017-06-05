04308d0f7d08cff11cf7ef671aa74de4b144c661
Sean Nam <namsangwoo1@gmail.com>
2017-06-05 00:57:39 -0700

    removed footer from parselighthouse.py file

--- parselighthouse.py
+++ parselighthouse.py
@@ -4,8 +4,8 @@ import json, csv
 # import os.path
 from pathlib import Path
 
-#def parseLighthouse():
-count = 3
+#def parseLighthouse(count):
+count = 10
 for x in range(count):
 	y = x + 1
 	filename = "./results{0}.json".format(y)
@@ -25,7 +25,6 @@ for x in range(count):
 		data3 =	["Speed Index", speedIndex]
 		data4 =	["Estimated Input Latency", estimatedInputLatency]
 		data5 =	["Time to Interactive", timeToInteractive]
-		#footer = ["***************************", ""]
 		newLine = ["", ""]
 
 		#output = "./output{0}.csv".format(y)
@@ -44,5 +43,4 @@ for x in range(count):
 			wr.writerow(data3)
 			wr.writerow(data4)
 			wr.writerow(data5)
-			#wr.writerow(footer)
 			wr.writerow(newLine)
