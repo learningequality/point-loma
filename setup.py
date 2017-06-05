3e98f48c4e6e36fa76abf191c1ff54d382cc593f
Sean Nam <namsangwoo1@gmail.com>
2017-06-05 00:56:50 -0700

    removed old code in setup

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
-# from setuptools import setup
-
-# setup( 
-# 	name='HelloWorld',
-# 	version='1.0',
-# 	py_modules=['hello'],
-# 	install_requires=[
-# 		'Click',
-# 	],
-# 	entry_points='''
-# 		[console_scripts]
-# 		hello=hello:cli
-# 	''',
-# )
-
 from setuptools import setup
 
 setup(
 	name='pointLoma',
 	version='0.2',
-	py_modules=['pointLoma', 'sh', 'parselighthouse'],
+	py_modules=['pointLoma', 'sh', 'path'],
 	install_requires=[
 		'sh', 'Click', 'pathlib'
 	],
