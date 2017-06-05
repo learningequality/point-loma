f2fad3507ca87e1cb36c5edb38794bb873886ee7
Sean Nam <namsangwoo1@gmail.com>
2017-06-04 21:22:17 -0700

    loop through count option, running lighthouse each time

--- pointLoma.py
+++ pointLoma.py
@@ -1,15 +1,33 @@
 import click, sh
 #from Naked.toolshed.shell import execute_js, muterun_js
+#from parselighthouse import parseLighthouse
 
 @click.command()
-@click.option('--headless', '-hl', multiple=True, default=True, is_flag=True, 
-		help='Run in headless mode')
+#@click.option('--headless', '-hl', default=True, is_flag=True, 
+#		help='Run in headless mode')
+#@click.option('--verbose', is_flag=True, help='Verbose logging')
+@click.option('--count', default=1, help='Number of tests to run')
 @click.argument('link')
 
-def cli(link, headless):
+def cli(link, count):
 	"""This script runs Lighthouse using the command line interface."""
-	click.echo("Runing lighthouse on {0}".format(link))
-	if headless:
-		sh.lighthouse(link, "--output-path", "./myfile.json", "output", "html", "--chrome-flags=", "--headless")
-	else:
-		sh.lighthouse(link, "--output-path", "./myfile.json", "output", "html")
+	click.echo("Running lighthouse on '{0}' ...".format(link))
+	
+	#chromeHeadless = sh.Command("/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary")
+	#chromeHeadless("--headless", "--remote-debugging-port=9222", "--disable-gpu")
+
+	# if headless:
+	# 	sh.lighthouse(link, "--output-path", "./myfile.json", "output", "html", "--chrome-flags=", "--headless")
+	# else:
+	# 	if verbose:
+	# 		sh.lighthouse(link, "--output-path", "./myfile.json", "output", "json", "--verbose")
+	# 	else:
+	# 		sh.lighthouse(link,  "--output", "json", "--output", "html")
+
+	#sh.lighthouse(link,  "--output", "json", "--output-path=", "./results.json")
+	for x in range(count):
+		y = x+1
+		click.echo("Test #{0}...".format(x+1))
+		
+		filename = "./results{0}.json".format(y)
+		sh.lighthouse(link, "--output", "json", "--output-path", filename)
