0bb8e9075a8c1c027861c1e8f2101117e5948452
bonidjukic <boni@djukic.com.hr>
2018-06-27 13:51:35 +0200

    Check if url is reachable before running

--- pointloma/core.py
+++ pointloma/core.py
@@ -4,6 +4,9 @@ import subprocess
 import sys
 import tempfile
 
+from urllib.request import urlopen
+from urllib.error import URLError
+
 from utils import parse_cli_opts
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -25,9 +28,16 @@ class PointLoma:
         self.workdir = self._get_workdir()
 
     def run(self):
+        url = self.opts.url
+
+        if not self._check_url(url):
+            self._log('error', 'No response from the url: {url}'.format(
+                url=url))
+            return
+
         for i in range(self.opts.runs):
             self._run_cmd(
-                self._get_lighthouse_cmd(self.opts.url))
+                self._get_lighthouse_cmd(url))
 
     def clean(self):
         self._remove_workdir()
@@ -45,6 +55,8 @@ class PointLoma:
             raise ValueError('Log type is required')
         if log_type not in ['info', 'error']:
             raise ValueError('Log type not supported')
+
+        msg = ' ' + msg
 
         if log_type == 'info' and self.opts.verbose:
             self.logger.info(msg)
@@ -71,3 +83,12 @@ class PointLoma:
                 '--output', 'json',
                 '--output-path', '/tmp/pl',
                 '--chrome-flags="--headless"']
+
+    def _check_url(self, url):
+        try:
+            urlopen(url)
+            return True
+        except ValueError:
+            return False  # URL not well formatted
+        except URLError:
+            return False  # URL doesn't seem to be reachable
